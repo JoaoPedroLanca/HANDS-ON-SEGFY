a5ff3af0050fb6263c6129aeea0e6999ce64996c
João Pedro Lança <joaolanca53@gmail.com>
2026-07-01 16:25:41 -0300

    feat: Adiciona campo que estava faltando ao dto

--- DesafioSegfy/Api/Dtos/ApoliceResponse.cs
+++ DesafioSegfy/Api/Dtos/ApoliceResponse.cs
@@ -9,6 +9,7 @@ namespace DesafioSegfy.Api.Dtos
     {
         public Guid Id { get; set; }
         public string NumeroApolice { get; set; } = string.Empty;
+        public string CpfCnpj { get; set; } = string.Empty;
         public string Placa { get; set; } = string.Empty;
         public decimal ValorPremio { get; set; }
         public DateOnly DataInicioVigencia { get; set; }
